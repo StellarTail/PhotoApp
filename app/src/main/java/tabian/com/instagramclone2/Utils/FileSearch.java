3b01b1f0f1718a2d135be107ec06a4b5fb27db3d
Feng Zhao <w441@me.com>
2018-10-17 22:43:52 +1100

    Add authentication and bottom navi bar
    Add authentication and bottom navi bar

--- app/src/main/java/tabian/com/instagramclone2/Utils/FileSearch.java
+++ app/src/main/java/tabian/com/instagramclone2/Utils/FileSearch.java
@@ -20,9 +20,11 @@ public class FileSearch {
         ArrayList<String> pathArray = new ArrayList<>();
         File file = new File(directory);
         File[] listfiles = file.listFiles();
-        for(int i = 0; i < listfiles.length; i++){
-            if(listfiles[i].isDirectory()){
-                pathArray.add(listfiles[i].getAbsolutePath());
+        if(listfiles!=null){
+            for(int i = 0; i < listfiles.length; i++){
+                if(listfiles[i].isDirectory()){
+                    pathArray.add(listfiles[i].getAbsolutePath());
+                }
             }
         }
         return pathArray;
@@ -37,9 +39,11 @@ public class FileSearch {
         ArrayList<String> pathArray = new ArrayList<>();
         File file = new File(directory);
         File[] listfiles = file.listFiles();
-        for(int i = 0; i < listfiles.length; i++){
-            if(listfiles[i].isFile()){
-                pathArray.add(listfiles[i].getAbsolutePath());
+        if(listfiles!=null){
+            for(int i = 0; i < listfiles.length; i++){
+                if(listfiles[i].isFile()){
+                    pathArray.add(listfiles[i].getAbsolutePath());
+                }
             }
         }
         return pathArray;
